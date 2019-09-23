a7667b2304e43b7f97fb807f25d8ff59164e2e9d
iagojorge <55568314+iagojorge@users.noreply.github.com>
2019-09-23 16:19:51 -0300

    Update main.java
    Agora a mediana trabalha com a amostra ordenada.

--- src/main.java
+++ src/main.java
@@ -190,10 +190,12 @@ public class main {
 	 * divididos por dois.
 	 */
 	private static int mediana(int[] amostra) {
+		int amostraOrdenada[] = ordenarAmostra(amostra);
+
 		if (amostra.length % 2 == 0) { // par
-			return ((amostra[(amostra.length / 2)] + amostra[(amostra.length / 2) + 1]) / 2);
+			return ((amostraOrdenada[(amostra.length / 2)] + amostraOrdenada[(amostra.length / 2) - 1]) / 2);
 		} else { // impar
-			return amostra[amostra.length / 2]; 
+			return amostraOrdenada[amostra.length / 2];
 		}
 	}
 
