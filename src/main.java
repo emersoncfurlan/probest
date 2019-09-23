e83ba80558bef3b2846ec8fc8188217af30528ee
Emerson Chiabai Furlan <emersonfurlanpc@gmail.com>
2019-09-22 23:41:08 -0300

    Menu inicial afim de apresentar a ideia do sistema para o professor
    Segue o menu inicial criado com JOption Pane, será necessário ainda correção de vários métodos bem como estruturar melhor o menu, quem sabe talvez criando submenus.

--- src/main.java
+++ src/main.java
@@ -1,18 +1,98 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.Random;
+
+import javax.swing.JOptionPane;
 
 public class main {
+	private static final int SOMATORIO = 1;
+	private static final int MEDIA_ARITMETICA_SIMPLES = 2;
+	private static final int MEDIA_ARITMETICA_PONDERADA = 3;
+	private static final int MODA = 4;
+	private static final int MEDIANA = 5;
+	private static final int VARIANCIA = 6;
+	private static final int DESVIO_PADRAO = 7;
+	private static final int MEDIA_GEOMETRICA = 8;
+	private static final int MEDIA_HARMONICA = 9;
+	private static final int PRODUTO = 10;
+	private static final int PROBABILIDADE = 11;
+	private static final int FATORIAL = 12;
+	private static final int SAIR_DO_PROGRAMA = 13;
 
 	public static void main(String[] args) {
-		// Variaveis de ambiente
-		int[] amostra1 = { 36, 35, 34, 38, 36, 38, 41 };
+		int[] amostra = { 36, 35, 34, 38, 36, 38, 41 };
 		int[] pesos = { 0, 1, 2, 2, 1, 0, 3, 4 };
-		// Métodos e funções
+		int possibilidade = 2;
+		Integer opcao = menu();
 
-		// mediaAritmeticaSimples(amostra1);
-		// mediaAritmeticaPonderada(amostra1, pesos);
-		moda(amostra1);
-		System.out.print(somatorio(amostra1));
+		while (opcao != SAIR_DO_PROGRAMA) {
+			switch (opcao) {
+			case SOMATORIO:
+				JOptionPane.showConfirmDialog(null, somatorio(pesos));
+				break;
+			case MEDIA_ARITMETICA_SIMPLES:
+				JOptionPane.showConfirmDialog(null, mediaAritmeticaSimples(amostra));
+				break;
+			case MEDIA_ARITMETICA_PONDERADA:
+				JOptionPane.showConfirmDialog(null, mediaAritmeticaPonderada(amostra, pesos));
+				break;
+			case MODA:
+				JOptionPane.showConfirmDialog(null, moda(amostra));
+				break;
+			case MEDIANA:
+				JOptionPane.showConfirmDialog(null, mediana(amostra));
+				break;
+			case VARIANCIA:
+				JOptionPane.showConfirmDialog(null, variancia(amostra));
+				break;
+			case DESVIO_PADRAO:
+				JOptionPane.showConfirmDialog(null, desvioPadrao(amostra));
+				break;
+			case MEDIA_GEOMETRICA:
+				JOptionPane.showConfirmDialog(null, mediaGeometrica(amostra));
+				break;
+			case MEDIA_HARMONICA:
+				JOptionPane.showConfirmDialog(null, mediaHarmonica(amostra));
+				break;
+			case PRODUTO:
+				JOptionPane.showConfirmDialog(null, produto(amostra));
+				break;
+			case PROBABILIDADE:
+				JOptionPane.showConfirmDialog(null, probabilidade(amostra, possibilidade));
+				break;
+			case FATORIAL:
+				JOptionPane.showConfirmDialog(null, fatorial(possibilidade));
+				break;
+			case SAIR_DO_PROGRAMA:
+				JOptionPane.showMessageDialog(null, "[Programa ser� encerado!]");
+				break;
+			default:
+				JOptionPane.showMessageDialog(null, "Opcao Invalida!");
+				break;
+			}
+			opcao = menu();
+		}
+	}
+
+	private static Integer menu() {
+		String menu = "[-------- Menu ---------]";
+		menu += "\n[1]  - Somatorio";
+		menu += "\n[2]  - Media Aritmetica Simples";
+		menu += "\n[3]  - Media Aritmetica Ponderada";
+		menu += "\n[4]  - Moda";
+		menu += "\n[5]  - Mediana";
+		menu += "\n[6]  - Variancia";
+		menu += "\n[7]  - Desvio Padrao";
+		menu += "\n[8]  - Media Geometrica";
+		menu += "\n[9]  - Media Harmonica";
+		menu += "\n[10] - Produto";
+		menu += "\n[11] - Probabilidade";
+		menu += "\n[12] - Fatorial";
+		menu += "\n[13] - Sair";
+		menu += "\n[ ------------------------- ]";
+		menu += "\nInforme sua opcao: ";
+
+		String strOpcao = JOptionPane.showInputDialog(null, menu);
+
+		return Integer.parseInt(strOpcao);
 	}
 
 	private static int somatorio(int[] amostra) {
@@ -23,8 +103,17 @@ public class main {
 		return somatorio;
 	}
 
+	private static int mediaHarmonica(int[] amostra) {
+		int mediaHarm = amostra.length;
+		int somat = 0;
+		for (int i = 1; i < amostra.length; i++) {
+			somat = 1 / amostra[i];
+		}
+		return mediaHarm / somat;
+	}
+
 	/*
-	 * Ms: média aritmética simples x1, x2, x3,...,xn: valores dos dados n: número
+	 * Ms: m�dia aritm�tica simples x1, x2, x3,...,xn: valores dos dados n: numero
 	 * de dados
 	 */
 	private static int mediaAritmeticaSimples(int[] amostra) {
@@ -32,9 +121,9 @@ public class main {
 	}
 
 	/*
-	 * A média aritmética ponderada é calculada multiplicando cada valor do conjunto
-	 * de dados pelo seu peso. Depois, encontra-se a soma desses valores que será
-	 * dividida pela soma dos pesos Mp: Média aritmética ponderada p1, p2,..., pn:
+	 * A m�dia aritm�tica ponderada � calculada multiplicando cada valor do conjunto
+	 * de dados pelo seu peso. Depois, encontra-se a soma desses valores que ser�
+	 * dividida pela soma dos pesos Mp: M�dia aritm�tica ponderada p1, p2,..., pn:
 	 * pesos x1, x2,...,xn: valores dos dados
 	 */
 	private static int mediaAritmeticaPonderada(int[] amostra, int[] pesos) {
@@ -73,7 +162,7 @@ public class main {
 	/*
 	 * A Moda (Mo) representa o valor mais frequente de um conjunto de dados, sendo
 	 * assim, para defini-la basta observar a frequência com que os valores
-	 * aparecem. Um conjunto de dados é chamado de bimodal quando apresenta duas
+	 * aparecem. Um conjunto de dados � chamado de bimodal quando apresenta duas
 	 * modas, ou seja, dois valores são mais frequentes.
 	 */
 	private static int moda(int[] amostra) {
@@ -85,7 +174,7 @@ public class main {
 		// verifica frequencia dos numeros
 		for (int i = 0; i < amostraOrdenada.length; i++) {
 			// percorre o vetor j a cada item do vetor i para contabilizar,
-			// como a amostra está ordenada se o item posterior for diferente do item atual
+			// como a amostra est� ordenada se o item posterior for diferente do item atual
 			// não tem porque voltar e rever o item
 			for (int j = 0; j < amostraOrdenada.length; j++) {
 
@@ -96,9 +185,9 @@ public class main {
 	}
 
 	/*
-	 * Quando o número elementos de um conjunto é par, a mediana é encontrada pela
-	 * média dos dois valores centrais. Assim, esses valores são somados e divididos
-	 * por dois.
+	 * Quando o n�mero elementos de um conjunto � par, a mediana � encontrada pela
+	 * m�dia dos dois valores centrais. Assim, esses valores são somados e
+	 * divididos por dois.
 	 */
 	private static int mediana(int[] amostra) {
 		if (amostra.length % 2 == 0) { // par
@@ -109,11 +198,11 @@ public class main {
 	}
 
 	/*
-	 * Variância é uma medida de dispersão e é usada também para expressar o quanto
-	 * um conjunto de dados se desvia da média. ∑: símbolo de somatório. Indica que
-	 * temos que somar todos os termos, desde a primeira posição (i=1) até a posição
-	 * n xi: valor na posição i no conjunto de dados MA: média aritmética dos dados
-	 * n: quantidade de dados
+	 * Variância � uma medida de dispersão e � usada tamb�m para expressar o
+	 * quanto um conjunto de dados se desvia da m�dia. ∑: símbolo de somatório.
+	 * Indica que temos que somar todos os termos, desde a primeira posição (i=1)
+	 * at� a posição n xi: valor na posição i no conjunto de dados MA: m�dia
+	 * aritm�tica dos dados n: quantidade de dados
 	 */
 	private static int variancia(int[] amostra) {
 		int ma = mediaAritmeticaSimples(amostra);
@@ -133,7 +222,7 @@ public class main {
 		return Math.sqrt(variancia);
 	}
 
-	// Media Geométrica
+	// Media Geom�trica
 	private static double mediaGeometrica(int[] amostra) {
 		double mg = 0;
 		int produto = 0;
@@ -152,13 +241,13 @@ public class main {
 
 	// Amplitude
 	private static int amplitude(int[] amostra) {
-		// Essa medida de dispersão é definida como a diferença entre a maior e a menor
-		// observação de um conjunto de dados, isto é:
+		// Essa medida de dispers�o � definida como a diferenca entre a maior e a menor
+		// observa��o de um conjunto de dados, isto �:
 		int a = amostra[0] - amostra[amostra.length];
 		return a;
 	}
 
-	// Coeficiente de Variação
+	// Coeficiente de Varia��o
 	private static double coeficienteDeVariacao(int[] amostra) {
 		double cv = (desvioPadrao(amostra) * 100) / mediaAritmeticaSimples(amostra);
 		return cv;
@@ -166,20 +255,21 @@ public class main {
 
 	// Formula da Probabilidade
 	/*
-	 * Em um fenômeno aleatório, as possibilidades de ocorrência de um evento são
-	 * igualmente prováveis. p(A): probabilidade da ocorrência de um evento A n(A):
-	 * número de casos que nos interessam (evento A) n(Ω): número total de casos
+	 * Em um fen�meno aleatório, as possibilidades de ocorrência de um evento são
+	 * igualmente prov�veis. p(A): probabilidade da ocorrência de um evento A n(A):
+	 * n�mero de casos que nos interessam (evento A) n(Ω): n�mero total de casos
 	 * possíveis
 	 */
 	private static double probabilidade(int[] amostra, int possibilidade) {
 		// Sendo assim, podemos encontrar a probabilidade de ocorrer um determinado
-		// resultado através da divisão
-		// entre o número de eventos favoráveis e o número total de resultados
+		// resultado atrav�s da divisão
+		// entre o n�mero de eventos favor�veis e o n�mero total de resultados
 		// possíveis:
 		long numTotal = fatorial(amostra.length);
 		long pa = possibilidade / numTotal;
 		// converter para porcentagem aproximada
 		double porcent = 100 * (double) pa;
+		JOptionPane.showConfirmDialog(null, porcent + "%");
 		return porcent;
 	}
 
@@ -187,11 +277,12 @@ public class main {
 	private static long fatorial(int num) {
 		int anterior = num - 1;
 		long fatorial = 0;
-		if (num <= 1)
+		if (num <= 1) {
 			return 1;
-		else
+		} else {
 			fatorial = num * fatorial(anterior);
-		return fatorial;
+			return fatorial;
+		}
 	}
 
 }
