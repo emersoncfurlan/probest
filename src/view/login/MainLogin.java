f1264dbbce5dca38e21245a0400008d95764dd55
Emerson Chiabai Furlan <emersonfurlanpc@gmail.com>
2019-10-15 23:21:49 -0300

    #11 - Tela de Login
    #11 - Tela de Login Criada
    Criado layout inicial, logo, entrada de texto para usuario, e entrada de texto para senha, além de um botão de login

---
+++ src/view/login/MainLogin.java
@@ -0,0 +1,46 @@
+package view.login;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class MainLogin extends Application {
+	private double xOffset = 0;
+	private double yOffset = 0;
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("profile.fxml"));
+
+		root.setOnMousePressed(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				xOffset = event.getSceneX();
+				yOffset = event.getSceneY();
+			}
+		});
+		root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				primaryStage.setX(event.getScreenX() - xOffset);
+				primaryStage.setY(event.getScreenY() - yOffset);
+			}
+		});
+
+		Scene scene = new Scene(root);
+
+		primaryStage.initStyle(StageStyle.TRANSPARENT);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
